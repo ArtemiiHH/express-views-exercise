ba2ea640178c64e82ea817f32f00f0fed8b23070
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-01 21:40:19 +0200

    Create views dir + display text using EJS

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
 const path = require("node:path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port:${PORT}`);
+});
