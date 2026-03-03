c068d7406b1723d525798906360c7f47531f65d5
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-03 22:36:38 +0200

    Changes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const { link } = require("node:fs");
 const app = express();
 const PORT = 3000;
 
 const path = require("node:path");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks" });
+  res.render("index", { link: links });
 });
 
 app.listen(PORT, () => {
